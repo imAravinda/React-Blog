68aef91361270b6c19e81a436de15f17798543df
iamAravinda <acfernando1999@gmail.com>
2021-07-07 23:06:37 +0530

    added to commit

---
+++ src/Home.js
@@ -0,0 +1,28 @@
+import {useState,useEffect} from 'react';
+import BlogList from './bloglist';
+const Home = () => {
+
+    const[blogs,setBlogs] = useState(null)
+    const[isPending, setIsPending] =useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetch('http://localhost:5000/blogs')
+                .then(res => {
+                return res.json();
+                })
+                .then(data => {
+                setBlogs(data);
+                setIsPending(false);
+                })
+        }, 100)
+    }, [])
+    return (  
+        <div className="home">
+            {isPending && <div>Loading...</div> }
+           {blogs && <BlogList blogs={blogs} title='All Blogs!'/>}
+        </div>
+    );
+}
+
+export default Home;
