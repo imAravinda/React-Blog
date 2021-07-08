c0675475ca43db2cd82856abc69972b49697df77
iamAravinda <acfernando1999@gmail.com>
2021-07-08 13:46:23 +0530

    import the create js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import Navbar from './navbar';
-import Home from './Home'
+import Home from './Home';
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import create from './create';
 function App() {
   return (
     <Router>
@@ -9,7 +10,7 @@ function App() {
       <Navbar />
       <div className="content">
         <Switch>
-          <Route path="/">
+          <Route exact path="/">
             <Home />
           </Route>
           <Route path="/create">
