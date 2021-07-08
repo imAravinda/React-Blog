fd559690b1f168a599575fac33eaea94c1f69548
iamAravinda <acfernando1999@gmail.com>
2021-07-09 00:06:15 +0530

    add blog details

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,8 +1,20 @@
 import { useParams } from "react-router";
+import useFetch from "./useFetch";
 const BlogDetails = () => {
     const {id} =useParams();
+    const {data:blog,isPending,error}=useFetch('http://localhost:5000/blogs/' + id);
     return ( 
-        <h2>Blog Details - {id}</h2>
+        <div className="blog-details">
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {blog && (
+                <article>
+                    <h2>{ blog.title }</h2>
+                    <p>writte by { blog.author }</p>
+                    <div>{ blog.body }</div>
+                </article>
+            )}
+        </div>
      );
 }
  
