6a0987ef26663bdd5981e425c9502efd2ff12df7
iamAravinda <acfernando1999@gmail.com>
2021-07-09 01:18:44 +0530

    add author

--- src/create.js
+++ src/create.js
@@ -1,18 +1,24 @@
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('yoshi');
     return ( 
         <div className="create">
             <h2>Add a New Blog</h2>
             <form>
                 <label>Blog Title:</label>
-                <input type="text" required/>
+                <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} required/>
                 <label>Blog Body:</label>
-                <textarea required></textarea>
+                <textarea required value={ body } onChange={(e) => setBody(e.target.value)}></textarea>
                 <label>Blog author:</label>
-                <select>
+                <select value={author} onChange={(e) => setAuthor(e.target.value)}>
                     <option value="mario">Mario</option>
                     <option value="yoshi">Yoshi</option>
                 </select>
                 <button>Add Blog</button>
+                <p>{ title }</p>
+                <p>{ body }</p>
+                <p>{ author }</p>
             </form>
         </div>
      );
