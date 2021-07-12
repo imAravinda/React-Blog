c129197c8562d36355f9bc62e514ceda7d130e44
iamAravinda <acfernando1999@gmail.com>
2021-07-12 13:21:57 +0530

    add deleting blog method

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,8 +1,18 @@
 import { useParams } from "react-router";
+import { useHistory } from "react-router-dom";
 import useFetch from "./useFetch";
 const BlogDetails = () => {
     const {id} =useParams();
     const {data:blog,isPending,error}=useFetch('http://localhost:5000/blogs/' + id);
+    const history=useHistory();
+    const handleClick= () =>
+    {
+        fetch('http://localhost:5000/blogs' + blog.id,{
+            method: 'DELETE'
+        }).then(()=>{
+            history.push('/');
+        })
+    }
     return ( 
         <div className="blog-details">
             {isPending && <div>Loading...</div>}
@@ -12,6 +22,7 @@ const BlogDetails = () => {
                     <h2>{ blog.title }</h2>
                     <p>writte by { blog.author }</p>
                     <div>{ blog.body }</div>
+                    <button onClick={handleClick}></button>
                 </article>
             )}
         </div>
